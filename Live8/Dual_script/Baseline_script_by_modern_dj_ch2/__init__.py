90e7e4abfef2627c52bebdefd08129a62a8dca5a
Modern DJ <marc@modern.dj>
2013-08-01 10:57:10 -0400

    consolidation and rearranging - work was getting scattered again...

---
+++ Live8/Dual_script/Baseline_script_by_modern_dj_ch2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# DJNSM / @djnsm / djnsm.com -> datamafia.com
+# Free to use, please refer to the readme.py file for license , info, and the uncompromised enlightenment
+
+from Baseline_script_by_modern_dj_ch2 import Baseline_script_by_modern_dj_ch2
+
+def create_instance(c_instance):
+    return Baseline_script_by_modern_dj_ch2(c_instance)
---
+++ Live8/Dual_script/README.txt
@@ -0,0 +1,5 @@
+This is my working script for use with the Livid Instruments Base Controller via a 2 channel set up.
+
+There is some error reporting that was going weird and that has been suppressed int he code. 
+
+--M
